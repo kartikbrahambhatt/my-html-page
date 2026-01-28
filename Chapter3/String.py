8902bb3af6c11679c88334ec209046712c33cd4b
kartik brahambhatt <kartikbrahambhatt08@gmail.com>
2026-01-28 12:16:03 +0530

    k

--- Chapter3/String.py
+++
@@ -1,9 +0,0 @@
-name = "kartik"
-
-print (name [0:6])
-print (name [:6])
-print (name [0:])
-print (len(name))
-print (name.endswith("tik"))
-print (name.startswith("kar"))
-print(name.capitalize())
